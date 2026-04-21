036f3d79d678206566dc28719d503c94565b48bd
james <james362@gmail.com>
2026-04-20 21:12:18 -0500

    config properties

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   // apiUrl: 'http://localhost:8080/api'
-  apiUrl: 'https://minierp-backend-production.up.railway.app/api'
+  apiUrl: 'https://exemplary-energy-production-fed1.up.railway.app/api'
 };
